8e613cac364599961ac815506cf89dcb0f2a31e9
ephraimguo <ephraimguo@gmail.com>
2018-10-27 17:58:46 +0800

    update

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Richman
+//
+//  Created by Ephraim Guo Yanyu on 27/10/18.
+//  Copyright © 2018 Ephraim Guo Yanyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
